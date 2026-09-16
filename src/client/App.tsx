aef149c960d97c467e1ac2a2df402aa34c72bfa0
Waraku <153403020+Waraku1@users.noreply.github.com>
2026-09-16 23:12:26 +0900

    c4d: switch product entry to live account shell

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,11 @@
 import { FlightRuntime } from "./FlightRuntime";
+import { ProductLive } from "./product/ProductLive";
 import { ProductPreview } from "./product/ProductPreview";
 
 export default function App() {
-  const productPreview = new URLSearchParams(window.location.search).get("productPreview") === "1";
-  return productPreview ? <ProductPreview /> : <FlightRuntime />;
+  const params = new URLSearchParams(window.location.search);
+  const productPreview = params.get("productPreview") === "1";
+  const staticPreview = params.get("staticPreview") === "1";
+  if (productPreview) return staticPreview ? <ProductPreview /> : <ProductLive />;
+  return <FlightRuntime />;
 }
